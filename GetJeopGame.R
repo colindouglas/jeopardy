f90578cacd1b9c8f43d3f6e6de21be46c6b1d88e
Colin Douglas <colindouglas@gmail.com>
2018-02-07 23:36:55 -0400

    Handling games with negative scores

--- GetJeopGame.R
+++ GetJeopGame.R
@@ -2,8 +2,10 @@ library(rvest)
 library(tidyverse)
 library(stringr)
 
+malformedGames <- c()
+
 GetJeopGame <- function(gameID) {
-  Sys.sleep(1)
+  Sys.sleep(0.5)
   
   url <- paste0("http://www.j-archive.com/showgame.php?game_id=", gameID)
   
@@ -26,45 +28,56 @@ GetJeopGame <- function(gameID) {
     map(~ .[1]) %>%
     unlist()
   
-  scores <- html_scrape %>%
-    html_nodes(".score_positive") %>%
+ scores <- html_scrape %>%
+    html_nodes(xpath = '//*[contains(concat( " ", @class, " " ), concat( " ", "score_positive", " " ))] | //*[contains(concat( " ", @class, " " ), concat( " ", "score_negative", " " ))]') %>%
     html_text() %>%
     gsub('\\$|,', '', .) %>%
     as.numeric()
   
-#  return(data_frame(gameID, length = length(scores))) #debug
-
-  if(length(scores) == 15) {
-    gameInfo <- data_frame(gameID,
-                           episode = number,
-                           date,
-                           podium = factor(rep(1:3, 5), levels = 1:3),
-                           player = rep(players, 5),
-                           position = factor(c(
-                             rep("commercial", 3),
-                             rep("beforeDJ", 3),
-                             rep("beforeFJ", 3),
-                             rep("final",    3),
-                             rep("coryat", 3)), levels = c("commercial", "beforeDJ", "beforeFJ", "final", "coryat")),
-                           score = scores)
-    return(gameInfo)
-  } else {
-    return(data_frame(""))
-  }
+ #  return(data_frame(gameID, length = length(scores))) #debug
+ 
+ if(length(scores) == 15) {
+   gameInfo <- data_frame(gameID,
+                          episode = number,
+                          date,
+                          podium = factor(rep(1:3, 5), levels = 1:3),
+                          player = rep(players, 5),
+                          position = factor(c(
+                            rep("commercial", 3),
+                            rep("beforeDJ", 3),
+                            rep("beforeFJ", 3),
+                            rep("final",    3),
+                            rep("coryat", 3)), levels = c("commercial", "beforeDJ", "beforeFJ", "final", "coryat")),
+                          score = scores)
+   return(gameInfo)
+ } else {
+   malformedGames <- c(malformedGames, gameID)
+   return(data_frame(gameID,
+                     episode = NA,
+                     date = NA,
+                     podium = NA,
+                     player = NA,
+                     position = NA,
+                     score = NA)
+   )
+ }
+ 
 }
 
-firstGameID <- 5902
+games <- map_dfr(1:5902, ~ GetJeopGame(.))
 
-games <- map_dfr(5890:5902, ~ GetJeopGame(.))
-
-games %>%
-  filter(position != "coryat") %>%
-  ggplot(aes(x = position, y = score)) +
-  geom_point(aes(color = podium)) +
-  geom_smooth(aes(group = podium, color = podium))
-#  geom_line(aes(group = interaction(podium, episode), color = podium))
+write_csv(games, "data/all_games.csv")
 
 games %>%
   filter(position == "final") %>%
-  ggplot(aes(x = score)) +
-  geom_density(aes(fill = podium), alpha = 0.3)
+  ggplot(aes(x = podium, y = score)) +
+  geom_boxplot(aes(fill = podium))
+#  geom_line(aes(group = interaction(podium, episode), color = podium))
+# 
+# games %>%
+#   filter(position == "final") %>%
+#   ggplot(aes(x = score)) +
+#   geom_density(aes(fill = podium), alpha = 0.3)
+# 
+
+
